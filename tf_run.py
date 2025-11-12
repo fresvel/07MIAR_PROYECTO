de898a737094de627603d849e342a5d798e2ff46
fresvel <fresvel@outlook.com>
2025-11-12 08:27:05 -0500

    Multy trainer

--- tf_run.py
+++ tf_run.py
@@ -1,15 +1,22 @@
-#import os
-#os.environ["TF_XLA_FLAGS"] = "--tf_xla_auto_jit=0"
-#os.environ["TF_CPP_MIN_LOG_LEVEL"] = "2"
 from modulos.lemon_trainer import LemonTrainer, TrainerConfig
 
+# Configuración base
 cfg = TrainerConfig(
-    loader="tf",        # tf o "gen"
+    loader="tf",        # o "gen"
     mode="scratch",
     epochs=40,
     learning_rate=1e-3
 )
 
-trainer = LemonTrainer(cfg)
-results = trainer.run_trainer()
-results
+# Ejecutar 10 intentos de entrenamiento
+for i in range(1, 11):
+    attempt_id = f"{i:02d}"  # genera 01, 02, ..., 10
+    print(f"\n===== Iniciando entrenamiento intento {attempt_id} =====")
+
+    trainer = LemonTrainer(cfg, attempt=attempt_id)
+    results = trainer.run_trainer()
+
+    print("************************************************************************")
+    print(f"Entrenamiento {attempt_id} finalizado. Resultados obtenidos:")
+    print(results)
+    print("************************************************************************\n")
