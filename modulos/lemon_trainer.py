07541800431d4434169daf106ad59f0261b276e4
Ubuntu <ubuntu@ip-172-31-65-208.ec2.internal>
2026-01-12 20:00:51 +0000

    res smooth

--- modulos/lemon_trainer.py
+++ modulos/lemon_trainer.py
@@ -48,7 +48,7 @@ class TrainerConfig:
     patience_es: int = 20
     patience_rlrop: int = 8
     min_lr: float = 1e-6
-    save_dir: str = f"./res_{mode}_l2_1e_4"
+    save_dir: str = f"./res_{mode}_smooth"
     num_classes: int = 3
 
 
